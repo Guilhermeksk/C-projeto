cf80d29437b9913bf6e4dbe161cec00d5f4de6ea
Guilherme Pizzorno <80488512+Guilhermeksk@users.noreply.github.com>
2023-09-19 19:59:10 -0300

    a

--- asd.c
+++ asd.c
@@ -83,3 +83,92 @@ int main()
     return 0;
 }
 #endif ex2
+
+#ifdef ex3
+main(){
+int loop = 1;
+int diaA =0, mesA=0, anoA=0;
+int diaN=0, mesN=0, anoN=0;
+int nascBi = 0, atualBi = 0;
+int mesNPar = 0, mesAPar = 0;
+int fevN = 0, fevA = 0;
+
+	while(loop == 1){
+        diaA =0, mesA=0, anoA=0;
+        diaN=0, mesN=0, anoN=0;
+        nascBi = 0, atualBi = 0;
+        mesNPar = 0, mesAPar = 0;
+        fevN = 0, fevA = 0;
+        while(anoN == 0){
+            printf("Digite o ano do seu nascimento: ");
+            scanf("%i", &anoN);
+            }
+
+        if (anoN % 4 == 0){
+            printf("VOCE NASCEU EM ANO BISSEXTO!!\n");
+            nascBi = 1;
+        }
+
+        while(mesN < 1 || mesN >12){
+            printf("Digite o mes do seu nascimento: ");
+            scanf("%i", &mesN);
+        }
+
+        if (mesN == 1 ||mesN == 3 || mesN == 7 ||mesN == 8 ||mesN == 10 ||mesN == 12){
+            printf("31 dias\n");
+            mesNPar = 1;
+        }
+        if (mesN == 2){
+            fevN =- 2;
+        }
+        while(diaN < 1 || diaN > (30+fevN+mesNPar+nascBi)){
+        printf("Digite o dia do seu nascimento: ");
+        scanf("%i", &diaN);
+        }
+
+       while(anoA == 0){
+            printf("Digite o ano atual: ");
+            scanf("%i", &anoA);
+            }
+
+        if (anoA % 4 == 0){
+            printf("ESTAMOS EM ANO BISSEXTO!!\n");
+            atualBi = 1;
+        }
+
+        while(mesA < 1 || mesA >12){
+            printf("Digite o mes atual : ");
+            scanf("%i", &mesA);
+        }
+
+        if (mesA == 1 ||mesA == 3 || mesA == 7 ||mesA == 8 ||mesA == 10 ||mesA == 12){
+            printf("31 dias\n");
+            mesAPar = 1;
+        }
+
+        if (mesA == 2){
+            fevA = -2;
+        }
+        while(diaA < 1 || diaA > (30+fevA+mesAPar+atualBi)){
+        printf("Digite o dia atual: ");
+        scanf("%i", &diaA);
+        }
+
+        if( (diaA - diaN) < 1){
+            diaA += (30 + mesAPar+mesNPar);
+            mesA -1;
+        }
+/*            if( (mesA - mesN) < 1){
+            diaA + 30;
+            mesA -1;
+        }
+*/
+        printf("TOMA \n");
+        printf("Voce tem %i dias, %i meses e %i anos", diaA-diaN,mesA-mesN,anoA-anoN);
+
+        printf("\n1 para fazer denovo\n");
+        scanf("%i", &loop);
+    }
+}
+#endif
+
